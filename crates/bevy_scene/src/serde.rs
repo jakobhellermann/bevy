1fa15c8bfd801e07300e31644eb78a2b84bb3e6c
Jakob Hellermann <jakob.hellermann@protonmail.com>
2022-09-22 16:23:22 +0200

    fix explicit_auto_deref lint

--- crates/bevy_scene/src/serde.rs
+++ crates/bevy_scene/src/serde.rs
@@ -74,7 +74,7 @@ impl<'a> Serialize for ComponentsSerializer<'a> {
         for component in self.components {
             state.serialize_element(&ReflectSerializer::new(
                 &**component,
-                &*self.registry.read(),
+                &self.registry.read(),
             ))?;
         }
         state.end()
