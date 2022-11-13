ba14c71c8c24163c696cca3941ae69adb4c2fddc
Jakub Łabor <jacob.labor@gmail.com>
2022-11-13 02:26:42 +0100

    World Bundle API

--- crates/bevy_ecs/src/world/mod.rs
+++ crates/bevy_ecs/src/world/mod.rs
@@ -9,7 +9,7 @@ pub use world_cell::*;
 
 use crate::{
     archetype::{ArchetypeComponentId, ArchetypeId, Archetypes},
-    bundle::{Bundle, BundleInserter, BundleSpawner, Bundles},
+    bundle::{Bundle, BundleInfo, BundleInserter, BundleSpawner, Bundles},
     change_detection::{MutUntyped, Ticks},
     component::{
         Component, ComponentDescriptor, ComponentId, ComponentInfo, ComponentTicks, Components,
@@ -170,6 +170,17 @@ impl World {
     ) -> ComponentId {
         self.components
             .init_component_with_descriptor(&mut self.storages, descriptor)
+    }
+
+    pub fn init_bundle<'a, T: Bundle>(&'a mut self) -> &'a BundleInfo {
+        let components = &mut self.components;
+        let storages = &mut self.storages;
+        self.bundles.init_bundle::<T>(components, storages)
+    }
+
+    pub fn init_dynamic_bundle(&mut self, component_ids: Vec<ComponentId>) -> &BundleInfo {
+        let components = &mut self.components;
+        self.bundles.init_dynamic_bundle(components, component_ids)
     }
 
     /// Returns the [`ComponentId`] of the given [`Component`] type `T`.
